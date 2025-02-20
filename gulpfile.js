99b3bae0bf490d74de905a12642ecf8f7e171b4a
Phillip <contato.phillip.menezes@gmail.com>
2025-02-20 08:52:49 -0300

    #0001 - creating a hero section and importing font-family

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
+// const imagemin = require("gulp-imagemin");
 
 function formatStyles() {
   return gulp
@@ -9,8 +10,15 @@ function formatStyles() {
     .pipe(cleanCSS())
     .pipe(gulp.dest("./dist/css"));
 }
+// function imgMin() {
+//   return gulp
+//     .src("./src/images/**/*")
+//     .pipe(imagemin())
+//     .pipe(gulp.dest("./dist/images"));
+// }
 
-exports.default = formatStyles;
+exports.default = gulp.parallel(formatStyles);
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(formatStyles));
+  gulp.watch("./index.html", gulp.parallel(formatStyles));
 };
