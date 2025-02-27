9c38402570e28418bbb8d40fe28429b923e190e5
Phillip <contato.phillip.menezes@gmail.com>
2025-02-27 18:23:02 -0300

    #0005 imgmin fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 
 function formatStyles() {
@@ -8,18 +9,24 @@ function formatStyles() {
     .src("./src/styles/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(cleanCSS())
-    .pipe(gulp.dest("./dist/css"));
+    .pipe(gulp.dest("./dist/styles"));
 }
-function scriptsMin() {
+function imgMin() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
+function JSmin() {
   return gulp
     .src("./src/scripts/*.js")
     .pipe(uglify())
-    .pipe(gulp.dest("./dist/js"));
+    .pipe(gulp.dest("./dist/scripts"));
 }
 
-exports.default = gulp.parallel(formatStyles, scriptsMin);
+exports.default = gulp.parallel(formatStyles, imgMin, JSmin);
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(formatStyles));
   gulp.watch("./index.html", gulp.parallel(formatStyles));
-  gulp.watch("./src/scripts/*.js", gulp.parallel(scriptsMin));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(JSmin));
 };
