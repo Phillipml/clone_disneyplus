f22637131308007a382974b5db070f570c1264ae
Phillip <contato.phillip.menezes@gmail.com>
2025-02-27 17:28:52 -0300

    #0003 ajuste mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
+  const heroSection = document.querySelector(".hero");
+  const heroSectionHeight = heroSection.clientHeight;
+
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+
+    scrollPosition < heroSectionHeight ? hiddenHeader() : showHeader();
+  });
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (button) {
@@ -15,8 +24,18 @@ document.addEventListener("DOMContentLoaded", function () {
       tabButton.classList.add("shows__tabs__button--is--active");
     });
   }
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", openCloseAnswer);
+  }
 });
-
+function hiddenHeader() {
+  const headerSelector = document.querySelector(".header");
+  headerSelector.classList.add("header--is-hidden");
+}
+function showHeader() {
+  const headerSelector = document.querySelector(".header");
+  headerSelector.classList.remove("header--is-hidden");
+}
 function hideBorderButton() {
   const borderButtons = document.querySelectorAll("[data-tab-button]");
 
@@ -31,4 +50,9 @@ function hideAllTabs() {
   for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove("shows__list--is--active");
   }
+}
+function openCloseAnswer(questionSelector) {
+  const openCloseClass = "faq__questions__item--is-open";
+  const questionParent = questionSelector.target.parentNode;
+  questionParent.classList.toggle(openCloseClass);
 }
