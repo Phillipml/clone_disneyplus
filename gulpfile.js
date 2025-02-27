40a2d9c741bf7d7be3b435a2faf1a98b0039ae0e
Phillip <contato.phillip.menezes@gmail.com>
2025-02-27 17:52:31 -0300

    #0004 uglify added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
-// const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 function formatStyles() {
   return gulp
@@ -10,15 +10,16 @@ function formatStyles() {
     .pipe(cleanCSS())
     .pipe(gulp.dest("./dist/css"));
 }
-// function imgMin() {
-//   return gulp
-//     .src("./src/images/**/*")
-//     .pipe(imagemin())
-//     .pipe(gulp.dest("./dist/images"));
-// }
+function scriptsMin() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+}
 
-exports.default = gulp.parallel(formatStyles);
+exports.default = gulp.parallel(formatStyles, scriptsMin);
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(formatStyles));
   gulp.watch("./index.html", gulp.parallel(formatStyles));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(scriptsMin));
 };
